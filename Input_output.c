ed94f03c451719e503c31356c51e7a2b4888e914
Bagavathi <140944606+Bagavathi2002@users.noreply.github.com>
2024-10-26 12:11:00 +0530

    Create Input_output.c

---
+++ Input_output.c
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    string b,c;
+    cout << " Enter ID No: ";
+    cin >> a;
+    cout << "Enter Name: ";
+    cin >> b;
+    cout << "Enter Address: ";
+    cin >> c;
+    cout <<"The ID is: "<<a<<endl;
+    cout <<"The Name is: "<<b<<endl;
+    cout <<"The Address is: "<<c<<endl; 
+    return 0;
+}
